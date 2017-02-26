6a7449050a35f70ab321e968bf2a4aac695e99f9
andrii-kostyk <kostykandri@gmail.com>
2017-02-26 12:53:02 +0200

    amber conversation script

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -1,2 +1,83 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).on('ready page:load', function() {
+   if ( $("#conversation").length ) new Amber.Initialization();
+});
+
+var Amber = {};
+
+Amber.Initialization = function() {
+  var self = this;
+
+  $.extend(this, new Amber.ElementaryProcessor());
+  $.extend(this, new Amber.RecognitionProcessor(this));
+
+  this.processRecognizedText = function(text) {
+  	console.log(text)
+    self.performAction(text);
+  };
+
+  this.inprogress = function() {};
+
+  this.sendCommand = function(text) {
+  	self.performAction = self.inprogress;
+  	Requests.sendCommand(text,
+  	  function(response) {
+  	  	self.speak(response.message);
+        self.performAction = self.pendingAppeal;
+  	  },
+  	  function(response) {
+        self.speak(response.message);
+        self.performAction = self.pendingAppeal;
+  	  }  
+    );
+  };
+
+  this.pendingAppeal = function(text) {
+    if (text.match(/Амбер/)) {
+      self.speak("Wait a command");
+      self.performAction = self.sendCommand;
+    }
+  }
+
+  this.performAction = this.pendingAppeal;
+  this.listen();
+};
+
+Amber.RecognitionProcessor = function(amber) {
+  var self = this;
+  var recognition = new webkitSpeechRecognition();
+
+  recognition.continuous = true;
+  recognition.interimResults = true;
+  recognition.lang = "uk-UA";
+
+  recognition.onresult = function(event) { 
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) amber.processRecognizedText(event.results[i][0].transcript);
+    } 
+  };
+  
+  recognition.onerror = function(e) {
+    console.log("RecognitionProcessor error");
+  }
+
+  this.listen = function() {
+  	recognition.start();
+  };
+
+  this.complete = function() {
+  	recognition.stop();
+  };
+};
+
+Amber.ElementaryProcessor = function() {
+  var utterance = new SpeechSynthesisUtterance();
+  
+  utterance.lang = 'en-US';
+  utterance.onend = function () {};
+  utterance.onerror = function (e) {};
+
+  this.speak = function(text) {
+    utterance.text = text;
+    speechSynthesis.speak(utterance);
+  };
+};
